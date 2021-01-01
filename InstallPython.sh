b430fc08f32ed12612b91066b268a4e87dc96b70
shalitha <mdsrwijethilaka@gmail.com>
2021-01-01 20:12:52 +0530

    Update InstallPython.sh

--- InstallPython.sh
+++ InstallPython.sh
@@ -4,6 +4,7 @@ echo "==========================================================================
 echo "Installing Suricata"
 echo "================================================================================="
 sudo apt-get update
-sudo apt-get -y install python-pip
+sudo apt-get -y install python3-pip
 pip install --upgrade pip
 pip3 install PyYAML
+echo "Python installation completed"
